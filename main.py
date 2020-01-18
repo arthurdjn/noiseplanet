6161d37d23e8db35b4f95ab5246c6143efc0b084
smermet <samuel.mermet@ensg.eu>
2020-01-18 15:48:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ Code temporaire servant à lancer le script principal depuis la racine du projet
 """
 
 import subprocess
-subprocess.call("start python3 src/main.py")
+subprocess.call("start python3 ./src/main.py")
