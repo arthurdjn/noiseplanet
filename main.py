a8dcc919420a87373bc5be61069815e65932d5c6
smermet <samuel.mermet@ensg.eu>
2020-01-18 16:10:15 +0100

    Create main_tmp.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Code temporaire servant à lancer le script principal depuis la racine du projet
-
-"""
-
-import subprocess
-subprocess.call("start python3 ./src/main.py")
