99ef066229ca449bf747541c44c65e7c97fac459
Arthur Dujardin <art.dujardin@hotmail.fr>
2020-01-25 16:53:25 +0100

    sphinx import module

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-autodoc_mock_imports = ['numpy', 'pandas', 'json', 'osmnx', 'folium', 'matplotlib', 'webbrowser', 'pyproj', 'leuvenmapmatching']
+autodoc_mock_imports = ['numpy', 'pandas', 'json', 'osmnx', 'folium', 'matplotlib', 
+                        'webbrowser', 'pyproj', 'leuvenmapmatching',
+                        'networkx']
 
 
 # -- Project information -----------------------------------------------------
